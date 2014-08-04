e2e9d5a7ad5606d53b5517554153ebde5bd9acae
Robinlovelace <rob00x@gmail.com>
2014-08-04 23:37:33 +0100

    polishing the paper

--- models/microbenchmarking/speed-comparison.R
+++ models/microbenchmarking/speed-comparison.R
@@ -1,3 +1,8 @@
 library(microbenchmark)
 
 microbenchmark(source("models/simple/etsim.R"), source("models/simple/etsim-ipfp.R"), times = 3)
+
+microbenchmark(source("models/small-area/etsim.R"), source("models/small-area/etsim-ipfp.R"), times =3)
+
+microbenchmark(source("models/sheffield/etsim.R"), source("models/sheffield/etsim-ipfp.R"), times =1)
+ 
